d318c8f759de4a42b9634ca09f4f45b77f3d4e9f
Nir Levy <nir@segolbalev.com>
2026-09-17 17:02:53 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from "next/navigation"
-import { getServerSession } from "next-auth"
 
-export default async function Home() {
-  const session = await getServerSession()
-  if (session) {
-    redirect("/dashboard/service")
-  } else {
-    redirect("/login")
-  }
+export default function Home() {
+  redirect("/login")
 }
